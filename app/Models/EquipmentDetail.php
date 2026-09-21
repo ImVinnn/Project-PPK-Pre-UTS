533797fefb9097a3c3164834d524076726e079d0
ImVinnn <m.banyuputraekap@gmail.com>
2026-09-21 21:56:47 +0700

    Tambah facility_id ke fillable EquipmentDetail dan RoomDetail

--- app/Models/EquipmentDetail.php
+++ app/Models/EquipmentDetail.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Attributes\Fillable;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
-#[Fillable(['brand', 'model', 'stock_total', 'stock_unavailable'])]
+#[Fillable(['facility_id', 'brand', 'model', 'stock_total', 'stock_unavailable'])]
 class EquipmentDetail extends Model
 {
     protected $primaryKey = 'facility_id';
